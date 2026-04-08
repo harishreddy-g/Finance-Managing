93df0a1f690eb4262dd3e8e7bd426370780f739b
harishreddy-g <harishreddy.g23@gmail.com>
2026-04-08 10:34:03 +0530

    POST API practised

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const transactions = [
     { category: "Food", amount: 200, date: "2023-06-01" },
     { category: "Transport", amount: 50, date: "2023-06-02" },
@@ -11,7 +13,27 @@ app.get('/', function (req, res) {
     res.send("Server is running");
 });
 app.get('/transactions', function (req, res) {
-    res.json(transactions);
+    res.json({
+        success: true,
+        data: transactions,
+        message: "Transactions fetched successfully"
+
+    });
+});
+app.post('/transactions', function (req, res) {
+    const newTransaction = req.body;
+    if (!newTransaction.category || !newTransaction.amount || !newTransaction.date) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid fields"
+        });
+    }
+    transactions.push(newTransaction);
+    res.status(201).json({
+        success: true,
+        data: newTransaction,
+        message: "Transactions added successfully"
+    });
 });
 app.listen(3000, function () {
     console.log("server is running on port 3000");
