440e87688710c4d3ef890595dd90d89099f2a010
harishreddy-g <harishreddy.g23@gmail.com>
2026-04-18 22:15:17 +0530

    added login feature in backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const user = require('../models/User');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
@@ -34,3 +35,49 @@ exports.signup = async function (req, res) {
         });
     }
 };
+
+exports.login = async function (req, res) {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                status: false,
+                message: "All fields are required !!"
+            });
+        }
+        const userEmail = await User.findOne({ email });
+
+        if (!userEmail) {
+            return res.status(400).json({
+                success: false,
+                message: "User not Found"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, userEmail.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid password"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                id: userEmail._id,
+                name: userEmail.name,
+                email: userEmail.email
+            }
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+}
